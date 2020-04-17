04083c405822a270e94c7afb6de20e01e3a8169e
Carmine Lentisco <carminelentisco@gmail.com>
2020-04-17 18:52:35 +0200

    JS
    Aggiunto invio messaggio con il click.

--- script/main.js
+++ script/main.js
@@ -1,5 +1,30 @@
 jQuery(document).ready(function($) {
 
-    console.log('JS');
+    var messageTextBar = $('.input-message-text');
+    var iconMicSend = $('.icon');
+
+    messageTextBar.on('click', function() {
+        
+        iconMicSend.removeClass('fa-microphone').addClass('fa-paper-plane');
+
+    });
+
+    var messageBox = $('#message-box');
+
+    iconMicSend.on('click', function() {
+
+        var messageText = messageTextBar.val();
+        var myMessageTemplateClone = $('.template-my-message').clone()
+        
+        // Modifica del clone
+        myMessageTemplateClone.find('.text-message p').text(messageText);
+
+        messageBox.append(myMessageTemplateClone);
+
+        messageTextBar.val('');
+
+    } );
+
+    
 
 }); // <--------------------- End Page
