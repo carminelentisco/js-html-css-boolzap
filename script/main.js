db738b14aaab308b4adb3e192b76d5a2902c97f9
Carmine Lentisco <carminelentisco@gmail.com>
2020-04-18 03:52:28 +0200

    JS

--- script/main.js
+++ script/main.js
@@ -1,30 +1,38 @@
-jQuery(document).ready(function($) {
+jQuery( document ).ready( function($) {
 
-    var messageTextBar = $('.input-message-text');
+    /*********** Logica di invio messaggi ************/ 
+    
+    // Setup di base
+    var inputText = $('.input-message-text');
     var iconMicSend = $('.icon');
 
-    messageTextBar.on('click', function() {
-        
+    inputText.on('focus', function () {
         iconMicSend.removeClass('fa-microphone').addClass('fa-paper-plane');
-
     });
 
-    var messageBox = $('#message-box');
-
-    iconMicSend.on('click', function() {
-
-        var messageText = messageTextBar.val();
-        var myMessageTemplateClone = $('.template-my-message').clone()
-        
-        // Modifica del clone
-        myMessageTemplateClone.find('.text-message p').text(messageText);
-
-        messageBox.append(myMessageTemplateClone);
-
-        messageTextBar.val('');
-
-    } );
-
+    var messageBox = $('.message-box');
     
+    iconMicSend.on('click', function() {
+        var newInputText = inputText.val();
+        var templateSendMessage = $('.template .template-message').clone();
+        templateSendMessage.find('span:first-child').text(newInputText);
+        messageBox.append(templateSendMessage);
+        inputText.val('');
+        iconMicSend.removeClass('fa-paper-plane').addClass('fa-microphone');
+    });
+
+    inputText.keyup(function (e) {   
+        if ( e.which == 13 ) {
+            var newInputText = inputText.val();
+            inputText.val('');
+            if ( newInputText !== '' ) {
+                var templateSendMessage = $('.template .template-message').clone();
+                templateSendMessage.find('span:first-child').text(newInputText);
+                messageBox.append(templateSendMessage);
+                iconMicSend.removeClass('fa-paper-plane').addClass('fa-microphone');
+            }
+        }
+    });
 
 }); // <--------------------- End Page
+
