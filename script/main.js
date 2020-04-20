e3989edf1aad80f684f6b4e4715d51892c0dc7ad
Carmine Lentisco <carminelentisco@gmail.com>
2020-04-20 17:04:26 +0200

    JS

--- script/main.js
+++ script/main.js
@@ -1,6 +1,23 @@
+// Variabili 
+
+var nomeChat = [
+    'michele',
+    'fabio',
+    'samuele',
+    'alessandro b.',
+    'alessandro l.', 
+    'claudia', 
+    'davide', 
+    'federico'
+   ];
+
 jQuery( document ).ready( function($) {
 
-    /*********** Logica di invio messaggi ************/ 
+    /*
+     * 
+     * Logica di invio messaggi
+     * 
+     */
     
     // Setup di base
     var inputText = $('.input-message-text');
@@ -35,67 +52,24 @@ jQuery( document ).ready( function($) {
      */
 
     var iconSearch = $('.search label i');
-    var chatSearchBar = $('#search-input');
-    var nomeChat = [
-                     'Michele',
-                     'Fabio',
-                     'Samuele',
-                     'Alessandro B.',
-                     'Alessandro L.', 
-                     'Claudia', 
-                     'Davide', 
-                     'Federico'
-                    ];
+    var searchInput = $('#search-input');   
 
 
     iconSearch.on('click', function() {
-
-        var textSearchBar = chatSearchBar.val();
-
-        if ( textSearchBar !== '' ) {
-
-            $('.global-chat div').hide();
-
-            for ( var i = 0; i < nomeChat.length; i++ ) {
-                for ( var k = 0; k < nomeChat[i].length; k++) {     
-                    if ( nomeChat[i].includes(textSearchBar) ) {
-                        console.log(nomeChat[i]);
-                        
-                    }
-                }
-                
-
-
-                /*
-                for ( var k = 0; k <= nomeChat[i].length; k++) {
-
-                    if ( nomeChat[i].includes(k) ) {
-                        console.log('si');
-                    }
-                    
-                }
-                */
-                
-            }
-            
-        } else if ( textSearchBar == '' ) {
-
-                $('.global-chat div').show();
-
-        }
         
-
-        //for ( var i = 0; i <= textSearchBar.length; i++ ) {
-        //}
-        
+        chatSearch();
 
     } );
 
-    /*
-    var textSearchBar = chatSearchBar.val();
+    searchInput.keyup(function (e) {   
+        
+        if ( e.which == 13 ) {
 
-    console.log(textSearchBar);
-    */
+            chatSearch(); 
+
+        }   
+
+    });
     
 
 }); // <--------------------- End Page
@@ -136,4 +110,57 @@ function reciveMessageAppend () {
 
     }, 1000);
 
+}
+
+function chatSearch() {
+    var textSearchBar = $('#search-input').val().toLowerCase();
+    
+    if ( textSearchBar !== '' ) {
+
+        $('.global-chat .chat').hide();
+
+        for ( var i = 0; i < nomeChat.length; i++ ) {
+            if ( nomeChat[i].includes(textSearchBar) ) {
+                
+                if ( nomeChat[i] == 'michele') {
+
+                    $('.global-chat .chat.michele').show();
+
+                } else if ( nomeChat[i] == 'fabio' ) {
+
+                    $('.global-chat .chat.fabio').show();
+
+                } else if ( nomeChat[i] == 'samuele' ) {
+
+                    $('.global-chat .chat.samuele').show();
+
+                } else if ( nomeChat[i] == 'alessandro b.' ) {
+
+                    $('.global-chat .chat.alessandro-b').show();
+
+                } else if ( nomeChat[i] == 'alessandro l.' ) {
+
+                    $('.global-chat .chat.alessandro-l').show();
+
+                } else if ( nomeChat[i] == 'claudia' ) {
+
+                    $('.global-chat .chat.claudia').show();
+
+                } else if ( nomeChat[i] == 'davide' ) {
+
+                    $('.global-chat .chat.davide').show();
+
+                } else if ( nomeChat[i] == 'federico' ) {
+
+                    $('.global-chat .chat.federico').show();
+                }
+                
+            }
+        }
+        
+    } else if ( textSearchBar == '' ) {
+
+            $('.global-chat div').show();
+
+    }
 }
